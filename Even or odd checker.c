196ba701d4ee3e5dd88c63a6c736f89ce9600b8c
career catalyst <crrcatalyst@gmail.com>
2024-10-28 08:46:04 -0700

    Update Even or odd checker.c

--- Even or odd checker.c
+++ Even or odd checker.c
@@ -1,6 +1,17 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main()
-{
+int main() {
+    int number;
 
+    printf("Enter an integer: ");
+    scanf("%d", &number);
+
+    if (number % 2 == 0) {
+        printf("%d is even.\n", number);
+    } else {
+        printf("%d is odd.\n", number);
+    }
+
+    return 0;
 }
+ 
